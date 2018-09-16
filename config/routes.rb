b19b219c2220b0124e88ee33b3fe666bdf9a4fd3
Tuukka Turunen <tuukka.turunen@helsinki.fi>
2018-09-16 22:02:03 +0300

    tehtävät 1 ja 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+  root 'breweries#index'
+  get 'kaikki_bisset', to: 'beers#index'
+  get 'ratings', to: 'ratings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
